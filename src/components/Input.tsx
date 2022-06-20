d87824d8bb7fed97f7bf2855d7a2e3a0b0bdbcaa
alisamaltseva <alisamonstr18@gmail.com>
2022-06-20 21:33:14 +0200

    add initial user page

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,15 +1,39 @@
+import { Path, SubmitHandler, UseFormRegister, useForm } from 'react-hook-form'
 import styled from 'styled-components'
 
+import { FormInputs } from '../pages/user/[id]'
+
+const InputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding-bottom: 10px;
+`
 const StyledInput = styled.input`
   height: 30px;
+  max-width: 500px;
   padding: 5px;
   border-radius: 5px;
   border: 1px solid #d0d0d0;
+  outline: 0;
 `
-export const Input = () => {
+const InputName = styled.label`
+  padding: 2px 0;
+  font-size: 10px;
+  font-weight: 600;
+  color: #aaaaaa;
+  text-transform: uppercase;
+`
+type InputProps = {
+  label: Path<FormInputs>
+  register: UseFormRegister<FormInputs>
+  required: boolean
+}
+
+export const Input = ({ label, register, required }: InputProps) => {
   return (
-    <div>
-      <StyledInput />
-    </div>
+    <InputContainer>
+      <InputName>{label}</InputName>
+      <StyledInput {...register(label, { required })} />
+    </InputContainer>
   )
 }
--- src/pages/user/[id].tsx
+++ src/pages/user/[id].tsx
@@ -1,28 +1,59 @@
 import { useRouter } from 'next/router'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import { useSelector } from 'react-redux'
 import styled from 'styled-components'
 
+import { Button } from '../../components/Button'
 import { Card } from '../../components/Card'
 import { Container } from '../../components/Container'
 import { Input } from '../../components/Input'
+import { usersSelector } from '../../selectors'
 
 const StyledContainer = styled(Container)`
   display: flex;
-  //align-items: center;
   justify-content: center;
 `
 const StyledCard = styled(Card)`
   width: 100%;
   max-width: 800px;
 `
+const StyledButton = styled(Button)`
+  margin: 10px 0;
+`
+
+export interface FormInputs {
+  email: string
+  phone: string
+  street: string
+  postalCode: string
+  city: string
+}
+
 const User = () => {
   const router = useRouter()
   const { id } = router.query
+  const { users } = useSelector(usersSelector)
+  const user = users?.find((u) => u.id === Number(id))
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<FormInputs>()
+  const onSubmit: SubmitHandler<FormInputs> = (data) => console.log(data)
 
   return (
     <StyledContainer>
       <StyledCard>
-        User: {id}
-        <Input />
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Input label="email" register={register} required />
+          <Input label="phone" register={register} required />
+          <Input label="street" register={register} required />
+          <Input label="postalCode" register={register} required />
+          <Input label="city" register={register} required />
+          <StyledButton type="submit"> Save Changes </StyledButton>
+        </form>
       </StyledCard>
     </StyledContainer>
   )
